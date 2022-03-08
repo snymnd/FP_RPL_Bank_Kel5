c5a7308eb403ab02386aa160069a3f7bad0ed224
reynerfernaldi <reynerfernaldi@gmail.com>
2022-03-08 22:37:59 +0700

    menambahkan CRUD user

--- src/models/Task.model.js
+++ src/models/Task.model.js
@@ -1,16 +1,44 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+        validator: function(v) {
+            return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please enter a valid email"
+    },
+    required: [true, "Email required"]
+  },
   name: {
     type: String,
-    required: [true, "must provide name"],
-    trim: true,
-    maxlength: [20, "name can not be more than 20 characters"],
+    required : [true, 'Nama Harus diisi!']
   },
-  completed: {
-    type: Boolean,
-    // default: false
+  NIK:{
+    type: Number,
   },
+  alamat:{
+    type: String
+  },
+  noTelp:{
+    type: String
+  },
+  password:{
+    type: String
+  },
+  saldo:{
+    type: Number
+  },
+  noRek:{
+    type:String
+  },
+  pinATM:{
+    type: String
+  }
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
